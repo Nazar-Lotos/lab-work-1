4835177664e71167714cc61f15aab118b86d5349
Nazar Lotos <lototskyinazar@gmail.com>
2024-08-25 15:16:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lab_work_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Television tv = new Television();
+
+            Console.Write("Введіть бренд телевізора: ");
+            tv.Brand = Console.ReadLine();
+
+            Console.Write("Введіть модель телевізора: ");
+            tv.Model = Console.ReadLine();
+
+            Console.Write("Введіть діагональ екрану телевізора (в дюймах): ");
+            tv.ScreenSize = int.Parse(Console.ReadLine());
+
+            Console.Write("Введіть роздільну здатність телевізора: ");
+            tv.Resolution = Console.ReadLine();
+
+            Console.Write("Це Smart TV (введіть true або false): ");
+            tv.IsSmartTV = bool.Parse(Console.ReadLine());
+
+            Console.Write("Введіть потужність звуку телевізора (у ватах): ");
+            tv.SoundPower = int.Parse(Console.ReadLine());
+
+            tv.TurnOn();
+
+            tv.DisplayInfo();
+
+            tv.TurnOff();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Television.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab_work_1
+{
+    class Television
+    {
+        public string Brand;
+        public string Model;
+        public int ScreenSize;
+        public string Resolution;
+        public bool IsSmartTV;
+        public int SoundPower;
+        public bool IsOn;
+
+        public void TurnOn()
+        {
+            IsOn = true;
+            Console.WriteLine("Телевізор увімкнено.");
+        }
+
+        public void TurnOff()
+        {
+            IsOn = false;
+            Console.WriteLine("Телевізор вимкнено.");
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Бренд: {Brand}");
+            Console.WriteLine($"Модель: {Model}");
+            Console.WriteLine($"Діагональ екрану: {ScreenSize}\"");
+            Console.WriteLine($"Роздільна здатність: {Resolution}");
+            Console.WriteLine($"Smart TV: {(IsSmartTV ? "Так" : "Ні")}");
+            Console.WriteLine($"Потужність звуку: {SoundPower} Вт");
+            Console.WriteLine($"Стан: {(IsOn ? "Увімкнено" : "Вимкнено")}");
+        }
+    }
+}
